1013f980f2eb85ce1023f6702f33287bb5d59c66
carlos-talavera <carloseduardorivera12@gmail.com>
2026-04-21 19:10:16 -0600

    chore(backend, deps): scaffold monorepo structure and add project dependencies

---
+++ apps/backend/src/main/java/com/charlie2code/bravotechnicalassessment/BravoTechnicalAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.charlie2code.bravotechnicalassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BravoTechnicalAssessmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BravoTechnicalAssessmentApplication.class, args);
+    }
+
+}
---
+++ apps/backend/src/test/java/com/charlie2code/bravotechnicalassessment/BravoTechnicalAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charlie2code.bravotechnicalassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BravoTechnicalAssessmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
